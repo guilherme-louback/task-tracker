d5bc99d42eb34c3b8ee778eeab6fa38d598255b9
guilherme louback <luisguilhermels21@gmail.com>
2025-03-19 17:54:42 -0300

    Updated 'list' command | Added '-i' and '--id' options - They show the task ID

--- bin/index.js
+++ bin/index.js
@@ -7,22 +7,29 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-    .name('Task Tracker')
-    .description('A CLI tool built to help you track your tasks!')
+.name('Task Tracker')
+.description('A CLI tool built to help you track your tasks!')
     .version('1.0.0');
 
     // CLI commands
 program
     .command('list')
-    .description('List all tasks')
-    .action(async () => {
+    .description('list all tasks')
+    .option('-i, --id', 'shows the task id')
+    .action(async (options) => {
         try{
             const filePath = resolve('C:/Users/guilherme.souza/Documents/ESTUDO/node/taskTracker/test.json');
             const contents = await readFile(filePath, { encoding : 'utf8'});
             const json = JSON.parse(contents);
-            for(let i = 0; i <= json.tasks.length - 1; i++){
-                console.log(json.tasks[i].description);
-            }
+            if(options.id){
+                for(let i = 0; i <= json.tasks.length - 1; i++){
+                    console.log(json.tasks[i].description + `\nID: ${json.tasks[i].id}`);
+                }
+            } else {
+                for(let i = 0; i <= json.tasks.length - 1; i++){
+                    console.log(json.tasks[i].description);
+                }
+            }    
         }
         catch(err){
             console.error(err.message);
@@ -33,3 +40,10 @@ program
 program.parse(process.argv);
 
 const options = program.opts();
+
+// ADD
+// generate a random ID
+// allow user to add a description
+// allow user to add a status
+// save the date of the creation
+// track the last update of the task - default is 'none'
