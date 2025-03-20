d78c08fc86cf69d76e315782469e6439346b7106
guilherme louback <luisguilhermels21@gmail.com>
2025-03-20 06:53:47 -0300

    Updated index.js | Created a getFile() function to handle the access to the file (instead of declaring it in every command)

--- bin/index.js
+++ bin/index.js
@@ -6,21 +6,52 @@ const { resolve } = require('path');
 const { Command } = require('commander');
 const program = new Command();
 
+async function getFile(){
+    try{
+        const filePath = resolve('C:/Users/guilherme.souza/Documents/ESTUDO/node/taskTracker/test.json');
+        const contents = await readFile(filePath, { encoding : 'utf8' });
+        return contents;
+    }
+    catch(err){
+        console.error(err);
+    }
+}
+
 program
-.name('Task Tracker')
-.description('A CLI tool built to help you track your tasks!')
+    .name('Task Tracker')
+    .description('A CLI tool built to help you track your tasks!')
     .version('1.0.0');
 
     // CLI commands
 program
-    .command('add')
-    .description('add a new task')
-    .action(async () => {
+    .command('list')
+    .description('list all tasks')
+    .option('-i, --id', 'shows the task id')
+    .action(async (options) => {
         try{
-            const filePath = resolve('C:/Users/guilherme.souza/Documents/ESTUDO/node/taskTracker/test.json');
-            const contents = await readFile(filePath, { encoding : 'utf8'});
+            const contents = await getFile();
             const json = JSON.parse(contents);
-            console.log(json);
+            if(options.id){
+                for(let i = 0; i <= json.tasks.length - 1; i++){
+                    console.log(json.tasks[i].description + `\nID: ${json.tasks[i].id}`);
+                }
+            } else {
+                for(let i = 0; i <= json.tasks.length - 1; i++){
+                    console.log(json.tasks[i].description);
+                }
+            }
+        }
+        catch(err){
+            console.error(err.message);
+        }
+    });
+
+program
+    .command('add')
+    .description('add new tasks')
+    .action(async (options) => {
+        try{
+        
         }
         catch(err){
             console.error(err.message);
